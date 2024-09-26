3aaccdc45bc74fc9d05cb9a51d7ff84ec04742f0
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-09-26 10:15:58 -0400

    Minor JS Changes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var input = document.getElementById("input");
 const prompts = document.getElementById('prompts');
 
+function prompt(toSay, xfit){
+  var say = input.value;
+  const newPrompt = document.createElement('div');
+  newPrompt.className = 'prompt';
+  newPrompt.innerHTML = toSay;
+  input.value = '';
+  if (xfit){
+    input.style.width = fit-content;
+  }
+  prompts.prepend(newPrompt);
+}
+
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
-    console.log(input.value);
-    var say = input.value;
-    const newPrompt = document.createElement('div');
-    newPrompt.className = 'prompt';
-    newPrompt.innerHTML = "You inputted: \n<i>" + input.value + "</i>";
-    input.value = '';
-    prompts.prepend(newPrompt);
+    prompt("You inputted: \n<i>" + input.value + "</i>", true);
   }
 });
