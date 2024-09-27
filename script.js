1ea8a8b414a0bd48817c16fa06f8ffb3515f8ae6
Peter <106273197+Elistic@users.noreply.github.com>
2024-09-27 02:24:28 -0400

    Proper Sizing in X-Direction

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ function prompt(toSay, xfit){
   newPrompt.className = 'prompt';
   newPrompt.innerHTML = toSay;
   input.value = '';
-  if (xfit){
-    input.style.width = fit-content;
+  if (xfit || toSay == "You inputted: \n<i>" + "size in x direction" + "</i>"){
+    newPrompt.style.width = "fit-content";
+    newPrompt.style.alignSelf = "center";
   }
   prompts.prepend(newPrompt);
 }
 
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
-    prompt("You inputted: \n<i>" + input.value + "</i>", true);
+    prompt("You inputted: \n<i>" + input.value + "</i>", false);
   }
 });
