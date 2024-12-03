0d6a9f14707d64295997fcf5b4b756ae86495fed
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-12-03 09:00:27 -0500

    Temp .json Push

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// const fs = require('fs');
-// app.get('/readFile', (req, res) => {
-//   const filePath = './firebase.js';
-//   fs.readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error reading file');
-//     } else {
-//       res.send(data);
-//     }
-//   });
-// });
