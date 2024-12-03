ab4f3adfb2ae8caa7fccc901c4159c60e2cffcda
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-12-03 10:14:04 -0500

    Got Local Server Up :)

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-import data from './data.json' assert { type: 'json' };
-console.log(data);
+console.log("Hello world!");
+var adventure;
+fetch('./data.json').then(response => {
+    if(!response.ok) {throw new Error('lmao');}
+    return response.json();
+}).then(data => {
+    start(data)
+}).catch(error => console.error("asnd", error));
+
+function start(adventure){
+    const info = "Pereruaiodn aroiapopajda ppa kropakod apkd ak pakpa kpodpka dkpsao podkpaojsd";
+    const regex = /\b[Pp]\w*/g;
+    const matches = info.match(regex);
+    console.log(matches);
+
+    adventure = adventure.toString();
+    console.log(adventure);
+    var listOfRooms = adventure.match(/(\+.*?(?=\+)|\+.*?$)+?/gs);
+    console.log(listOfRooms);
+}
