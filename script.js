18d5b83635cda197ed590c4f5903397dc08c3040
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-12-05 10:16:46 -0500

    Read JSON as String

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ fetch('./data.json').then(response => {
     if(!response.ok) {throw new Error('lmao');}
     return response.json();
 }).then(data => {
-    start(data)
+    start(data.Adventure)
 }).catch(error => console.error("asnd", error));
 
 function start(adventure){
-    const info = "Pereruaiodn aroiapopajda ppa kropakod apkd ak pakpa kpodpka dkpsao podkpaojsd";
-    const regex = /\b[Pp]\w*/g;
-    const matches = info.match(regex);
-    console.log(matches);
-
-    adventure = adventure.toString();
-    console.log(adventure);
     var listOfRooms = adventure.match(/(\+.*?(?=\+)|\+.*?$)+?/gs);
-    console.log(listOfRooms);
+    console.log(listOfRooms); //List of room strings.
 }
