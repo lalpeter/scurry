fd6cad0aea64a81d648c48138a72b9c660cdb3a7
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-12-09 10:00:58 -0500

    Regex Name Parsing + Example

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ fetch('./data.json').then(response => {
 }).catch(error => console.error("asnd", error));
 
 function start(adventure){
-    var listOfRooms = adventure.match(/(\+.*?(?=\+)|\+.*?$)+?/gs);
+    const listOfRooms = adventure.match(/(\+.*?(?=\+)|\+.*?$)+?/gs);
     console.log(listOfRooms); //List of room strings.
+    let data = [];
+    for (const room of listOfRooms) {
+        const name = room.match(/^\+(.*?)$/m)[0].substring(1).toLowerCase();
+        console.log(name);
+        data[name] = [];
+        // while ((match = re.exec(rant))){
+        //     if (match[1]){
+        //         bWords.push(match[1]);
+        //     }
+        //     if (match[2]){
+        //         aWords.push(match[2]);
+        //     }
+        // }
+
+        // Sample REGEX Run
+
+        // const rant = "Would you rather have unlimited bacon but no more video games, or games, unlimited games, but no games?";
+        // const re = /\b(\w*b\w*)|(\w*a\w*)\b/g;
+        // let match;
+        // let aWords = [];
+        // let bWords = [];
+        // while ((match = re.exec(rant))){
+        //     if (match[1]){
+        //         bWords.push(match[1]);
+        //     }
+        //     if (match[2]){
+        //         aWords.push(match[2]);
+        //     }
+        // }
+        // console.log(`Words with b: ${bWords}`);
+        // console.log(`Words with a: ${aWords}`);
+
+    }
 }
